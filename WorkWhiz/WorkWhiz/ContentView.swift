fefc780bb40ab5b6af8909f41af3380d73a52887
Ivan Gamov <idgamov@gmail.com>
2024-03-09 19:22:09 +0200

    Setup project

---
+++ WorkWhiz/WorkWhiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkWhiz
+//
+//  Created by Ivan Gamov on 9.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkWhiz/WorkWhiz/WorkWhizApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkWhizApp.swift
+//  WorkWhiz
+//
+//  Created by Ivan Gamov on 9.03.24.
+//
+
+import SwiftUI
+
+@main
+struct WorkWhizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
